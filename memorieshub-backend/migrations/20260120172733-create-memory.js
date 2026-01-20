8d95b48bac01fa9322dcd2e36ace79f7b963b62d
Tom Salon <tom.salon62@gmail.com>
2026-01-20 18:42:56 +0100

    feat: add memory model

---
+++ memorieshub-backend/migrations/20260120172733-create-memory.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Memories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      s3_url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Memories');
+  }
+};
--- memorieshub-backend/models/memory.js
+++ memorieshub-backend/models/memory.js
@@ -3,26 +3,25 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Memory extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Memory, {
+      Memory.belongsTo(models.User, {
         foreignKey: 'user_id',
-        as: 'memories',
+        as: 'owner',
       });
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+  Memory.init({
+    user_id: DataTypes.INTEGER,
+    s3_url: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Memory',
   });
-  return User;
+  return Memory;
 };
